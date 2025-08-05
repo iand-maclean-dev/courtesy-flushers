f248a67041298b7f790cff39caed4c41bf773de8
iand-maclean-dev <iand_maclean@hotmail.com>
2025-08-05 10:30:15 -0300

    bug: Trying new technique to fix the bug on mobile chrome bottom bar   hiding part of the footer.
      - Added useEffect to App.jsx and created a new vh unit which is 1%
        of the actual viewport height.
      - We set this value on initial load and window resize.
      - We use the new vh unit to set the min-height on the appContainer
        and multiply by 100 to get the full height. Provide 1svh as a
        fallback.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFound from "./NotFound";
 import HomePage from "./pages/Home/Home";
@@ -8,6 +9,21 @@ import Navbar from "./components/layout/Navbar/Navbar";
 import Footer from "./components/layout/Footer/Footer";
 
 function App() {
+  useEffect(() => {
+    const setViewportHeight = () => {
+      // window.innerHeight gives the actual viewport height.
+      // We calculate 1% of it to create a value similar to the 'vh' unit.
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    // Set the value on initial load and on window resize.
+    window.addEventListener("resize", setViewportHeight);
+    setViewportHeight();
+
+    return () => window.removeEventListener("resize", setViewportHeight);
+  }, []);
+
   return (
     <Router>
       <div className="appContainer">
