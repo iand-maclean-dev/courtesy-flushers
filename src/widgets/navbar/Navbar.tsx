2fe519be67dea96fbfc9a41265ebfcd64581b706
iand-maclean-dev <iand_maclean@hotmail.com>
2025-08-09 01:21:14 -0300

    bugfix: Imported react on the new footer.tsx and new navbar.tsx.

--- src/widgets/navbar/Navbar.tsx
+++ src/widgets/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import Logo from "@/shared/ui/Logo";
